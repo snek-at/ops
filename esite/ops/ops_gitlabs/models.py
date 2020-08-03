ada0b669a57c0924e037dffbe98dfa38b7e529a8
schettn <nicoschett@icloud.com>
2020-08-03 03:25:16 +0200

    Add Basic GitLabs Page
    The basic gitlabs model has been
    implemented.

--- esite/ops/ops_gitlabs/models.py
+++ esite/ops/ops_gitlabs/models.py
@@ -35,15 +35,42 @@ from esite.bifrost.models import (
 from esite.bifrost.helpers import register_streamfield_block
 
 # > Sections
+@register_streamfield_block
+class _S_GitlabBlock(blocks.StructBlock):
+    name = blocks.CharBlock(
+        null=True, required=True, help_text="Name of the presentator"
+    )
+    url = blocks.URLBlock(
+        null=True,
+        required=True,
+        help_text="Important! Format https://www.domain.tld/xyz",
+    )
+    description = blocks.TextBlock(
+        null=True, required=False, help_text="Other information"
+    )
+    mode = blocks.ChoiceBlock(
+        choices=[("polp", "Principle of least privilege"), ("idc", "Open privilege"),]
+    )
+    pat = blocks.CharBlock()
 
 
 # > Pages
 class OpsGitlabsPage(Page):
     # Only allow creating HomePages at the root level
-    parent_page_types = ["wagtailcore.Page"]
+    parent_page_types = ["ops_scpages.OpsScpagesPage"]
+
+    gitlabs_section = fields.StreamField(
+        [("_S_GitlabBlock", _S_GitlabBlock(null=True, icon="fa-gitlab")),],
+        null=True,
+        blank=False,
+    )
+
+    graphql_fields = [
+        GraphQLStreamfield("gitlabs_section"),
+    ]
 
     main_content_panels = [
-        # StreamFieldPanel("sections"),
+        StreamFieldPanel("gitlabs_section"),
     ]
 
     content_panels = Page.content_panels + main_content_panels
